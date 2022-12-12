d59db80831020cb37f1f19a57807933ec7e2af4e
bomii1 <101382369+bomii1@users.noreply.github.com>
2022-12-12 17:31:55 +0900

    Update Noodle.java

--- Noodle.java
+++ Noodle.java
@@ -8,12 +8,15 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
+// 상속 사용 -> Search 클래스를 상속 받음
 public class Noodle extends Search {
 	
+	// 부모클래스인 Search 의 search 함수를 재정의
 	@Override
 	public void search(int foodNum) throws IOException, Exception {
-		super.search(foodNum);
+		super.search(foodNum); // 부모클래스의 search 함수 사용
 		
+		// foodNum 이 3일 경우에 파일을 읽음
 		if (foodNum == 3) {
 			File file = new File("C:\\Temp\\kalguksu.txt");
 			Scanner sc = new Scanner(new InputStreamReader(new FileInputStream(file), "UTF8"));
