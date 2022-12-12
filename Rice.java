994227cf992880fb2f073cd48620c638c2f514fa
bomii1 <101382369+bomii1@users.noreply.github.com>
2022-12-12 21:17:53 +0900

    Update Rice.java

--- Rice.java
+++ Rice.java
@@ -8,20 +8,24 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
+// Search 클래스를 상속받음
 public class Rice extends Search {
 
 	@Override
-	public void search(int foodNum) throws IOException, Exception {
-		super.search(foodNum);
+	// Search 클래스의 search 함수 재정의
+	public void search(int foodNum) throws IOException, Exception { // 함수를 부른 곳으로 예외를 던져 예외처리
+		super.search(foodNum); // 부모 클래스인 Search 클래스의 search 함수 사용
+		
+		// foodNum 이 1일때와 2일때로 나누어 다른 파일을 읽음
 		if (foodNum == 1) {
-			File file = new File("C:\\Temp\\bokumbob.txt");
+			File file = new File("C://eclips//selfstudy//practice//01//src//koFoods//bokumbob.txt");
 			Scanner sc = new Scanner(new InputStreamReader(new FileInputStream(file), "UTF-8"));
 			while (sc.hasNext()) {
 				String str = sc.nextLine();
 				System.out.println(str);
 			}
 		} else if (foodNum == 2) {
-			File file = new File("C:\\Temp\\bibimbob.txt");
+			File file = new File("C://eclips//selfstudy//practice//01//src//koFoods//bibimbob.txt");
 			Scanner sc = new Scanner(new InputStreamReader(new FileInputStream(file), "UTF8"));
 			while (sc.hasNext()) {
 				String str = sc.nextLine();
@@ -30,4 +34,3 @@ public class Rice extends Search {
 		} 
 	}
 }
-
