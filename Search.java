8a0f99e7b0c72d2efd61d50621ba85b9a9e8a564
bomii1 <101382369+bomii1@users.noreply.github.com>
2022-12-12 17:39:04 +0900

    Update Search.java

--- Search.java
+++ Search.java
@@ -2,8 +2,9 @@ package koFoods;
 
 import java.io.IOException;
 
+// Rice, Noodle 클래스의 부모 클래스
 public class Search {
-	public void search(int Num) throws IOException, Exception {
+	public void search(int Num) throws IOException, Exception { // 예외처리
 		System.out.println("-----------loading-----------");
 	}
 
